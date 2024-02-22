b578097c382803093b32f2fe42875914f5869892
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2024-02-22 12:30:24 +0100

    jalon web partie 2

---
+++ cpu_api/public/assets/JS/CPU.js
@@ -0,0 +1,12 @@
+class CPU
+{
+    constructor(cpu){
+        this.idCpu = cpu.idCpu;
+        this.brand = cpu.brand;
+        this.family = cpu.family;
+        this.model = cpu.model;
+        this.ghz = cpu.ghz;
+        this.price = cpu.price;
+    }
+}
+export {CPU}
---
+++ cpu_api/public/assets/JS/main.js
@@ -0,0 +1,32 @@
+import {CPU} from "./CPU.js";
+
+const cpuUrl = 'http://localhost:3000/api/cpus';
+
+const app = {
+    data() {
+        return {
+            listeCpu:[]
+        }
+    },
+    async mounted() {
+        let rep = await fetch(cpuUrl);
+        let repForm = await rep.json();
+
+        console.log(repForm);
+        
+        for (let cpu of repForm){
+            let c = new CPU(cpu);
+            this.listeCpu.push(c);
+        }
+        
+        console.log(this.listeCpu);
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+
+Vue.createApp(app).mount('#app');
